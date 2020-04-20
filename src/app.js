c184ee6c2013c3b65c9fbb720bbc03b33b0e7ff2
teokigi <teokigi@hotmail.com>
2020-04-20 22:48:16 +0100

    done all

--- src/app.js
+++ src/app.js
@@ -5,14 +5,26 @@ document.addEventListener('DOMContentLoaded', () => {
     el: "#app",
     data: {
         chores: [
-            {name: "Vacuuming", priority: "high"}
-        ]
+            {name: "Vacuuming", priority: "high"},
+            {name: "taking out the trash", priority:"low" }
+        ],
+        newToDo: "",
+        newPriority: ""
     },
     methods: {
         saveNewItem:function(){
-            const newChore = {name:this.name, priority:this.toDoPriority}
+            const newChore = {name:this.newToDo, priority:this.newPriority}
+            this.chores.push(newChore)
+            this.newToDo="";
+            this.newPriority=null;
         }
+        
     }
   
   });
 });
+// done - Render a list of todos from an array of strings.
+// done - Add the capability to add a new todo to the list.
+// done - Update the todos array to take in objects with keys of name and priority.
+// done - Add radio buttons to track the level of priority
+// todo - Give the user visual feedback on what todos are high priority
